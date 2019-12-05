89566dd592c68bf48a6c08853d495b8f9d4fd66c
Natanael da Silva <dasilvanatanael700@gmail.com>
2019-12-05 19:56:18 -0300

    Updates

---
+++ precache-manifest.7a0fa454560098e06ade7db65ec90084.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e17627c0a66956362d33c38f902fd49",
+    "url": "/NatanaelGSSilva/Pwa5.git/index.html"
+  },
+  {
+    "revision": "0c8523f2171643c9d8fb",
+    "url": "/NatanaelGSSilva/Pwa5.git/static/css/main.c165ce0a.chunk.css"
+  },
+  {
+    "revision": "2225ee9c79ee51b81826",
+    "url": "/NatanaelGSSilva/Pwa5.git/static/js/2.a49d53d4.chunk.js"
+  },
+  {
+    "revision": "0c8523f2171643c9d8fb",
+    "url": "/NatanaelGSSilva/Pwa5.git/static/js/main.85740a48.chunk.js"
+  },
+  {
+    "revision": "c9e38227352294cbbc62",
+    "url": "/NatanaelGSSilva/Pwa5.git/static/js/runtime-main.b63e3697.js"
+  }
+]);
